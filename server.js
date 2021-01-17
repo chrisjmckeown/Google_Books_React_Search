e70af0fcc6ab3d362c94147fc8c53f8a7d55bf2f
Chris Mckeown <chris.j.mckeown@hotmail.com>
2021-01-18 10:42:34 +1100

    mongo connection updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/GoogleBooksSearch");
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/GoogleBooksSearch", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Start the API server
 app.listen(PORT, function() {
